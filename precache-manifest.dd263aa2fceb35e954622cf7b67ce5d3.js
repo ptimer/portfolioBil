c31b8bcb9e05bd5fbd5df63ed3ca66f3584a97ae
Illia <kriminalmykriminal@gmail.com>
2020-06-03 10:49:46 +0300

    Updates

---
+++ precache-manifest.dd263aa2fceb35e954622cf7b67ce5d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad6e82712bbe5aa883b465d64b24ffd7",
+    "url": "/portfolioBil/index.html"
+  },
+  {
+    "revision": "14d41ee6bd0d97640ef8",
+    "url": "/portfolioBil/static/css/main.dc266dde.chunk.css"
+  },
+  {
+    "revision": "e964d273e4a51568f926",
+    "url": "/portfolioBil/static/js/2.bdff09ed.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/portfolioBil/static/js/2.bdff09ed.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "14d41ee6bd0d97640ef8",
+    "url": "/portfolioBil/static/js/main.9b7531a4.chunk.js"
+  },
+  {
+    "revision": "1905d47b11a8f8569793",
+    "url": "/portfolioBil/static/js/runtime-main.0c0b5805.js"
+  }
+]);
